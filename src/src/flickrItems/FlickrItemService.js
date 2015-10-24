0a30e9d37cf88bb63a55f16059fdc4421a393dd2
Andy White <andywhite87@gmail.com>
2015-10-24 22:06:47 +0100

    Add filters to clean up the representation of item info

--- src/src/flickrItems/FlickrItemService.js
+++ src/src/flickrItems/FlickrItemService.js
@@ -1,34 +1,74 @@
 (function(){
   'use strict';
 
-  angular.module('flickrItems')
-         .service('flickrItemService', ['$q', '$http', '$log', ItemService]);
+angular.module('flickrItems')
+       .service('flickrItemService', ['$q', '$http', '$filter', '$log', ItemService]);
 
   /**
-   * FlickerItems DataService
+   * FlickrItems DataService
    *
    * @returns {{loadAllItems: Function}}
    * @constructor
    */
-  function ItemService($q, $http, $log) {
+  function ItemService($q, $http, $filter, $log) {
 
     var url = 'https://api.flickr.com/services/feeds/photos_public.gne' +
               '?jsoncallback=JSON_CALLBACK' +
               '&format=json' +
               '&tags=potato';
 
-    // Adds a src attribute to each object in an array, based on that object's media.m property
-    function getHighResImages(items) {
+    function filterItems(items) {
+
+      var filters = {
+
+        // Adds a high-res src attribute to an item, based on that item's media.m property
+        src: function(item) {
+          var media = item.media.m;
+          var fileEnding = media.slice(-6);
+
+          if (fileEnding === '_m.jpg') {
+            var src = media.substring(0, media.length - 6) + media.slice(-4);
+            item.src = src;
+          }
+
+          return item;
+        },
+
+        // Removes the default Flickr email prefix and suffix from an item's author property
+        author: function(item) {
+          var authorPrefix = 'nobody@flickr.com (';
+          var authorStart = item.author.substring(0, authorPrefix.length);
+
+          if (authorStart === authorPrefix) {
+            item.author = item.author.replace(authorPrefix, '');
+            item.author = item.author.substring(0, item.author.length -1);
+          }
+
+          return item;
+        },
+
+        // Transforms an item's published property into a readable representation
+        published: function(item) {
+          var published = new Date(item.published);
+
+          if (typeof published.getMilliseconds === 'function') {
+            var daySuffixes = ["th", "st", "nd", "rd"];
+            var day = $filter('date')(published, 'dd');
+            var relevantDigits = (day < 30) ? day % 20 : day % 30; // From http://stackoverflow.com/a/24061264
+            var daySuffix = (relevantDigits <= 3) ? daySuffixes[relevantDigits] : daySuffixes[0];
+
+            item.published = day + daySuffix + $filter('date')(published, " MMM yyyy 'at' h:mm");
+          }
+
+          return item;
+        }
+
+      };
 
       angular.forEach(items, function(item) {
-        
-        var media = item.media.m;
-        var fileEnding = media.slice(-6);
-
-        if (fileEnding === '_m.jpg') {
-          var src = media.substring(0, media.length - 6) + media.slice(-4);
-          item.src = src;
-        }
+        item = filters.src(item);
+        item = filters.author(item);
+        item = filters.published(item);
       });
 
       return items;
@@ -41,9 +81,10 @@
 
             var items = response.data.items;
 
-            var editedItems = getHighResImages(items);
+            // Run the returned items through filters
+            var filteredItems = filterItems(items);
 
-            return editedItems;
+            return filteredItems;
 
           },
           function errorCallback(response) {
