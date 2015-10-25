ba7d6a4c0b8fedf85bc9acddb85990c6370984f1
Andy White <andywhite87@gmail.com>
2015-10-25 18:27:50 +0000

    Expanded comments in FlickrItemFilters

--- src/src/flickrItems/FlickrItemFilters.js
+++ src/src/flickrItems/FlickrItemFilters.js
@@ -8,9 +8,11 @@
 
       return function(item) {
 
+        // Get the last six characters from the media.m property for comparison
         var media = item.media.m;
         var fileEnding = media.slice(-6);
 
+        // If it ends in '_m.jpg', remove the '_m' part to create a link to a higher-res version
         if (fileEnding === '_m.jpg') {
           var src = media.substring(0, media.length - 6) + media.slice(-4);
           item.src = src;
@@ -27,9 +29,11 @@
 
       return function(item) {
 
+        // Get the start of the item's author, based on the length of the prefix we're looking for
         var authorPrefix = 'nobody@flickr.com (';
         var authorStart = item.author.substring(0, authorPrefix.length);
 
+        // If it matches, remove the prefix and the last character from the author property
         if (authorStart === authorPrefix) {
           item.author = item.author.replace(authorPrefix, '');
           item.author = item.author.substring(0, item.author.length - 1);
@@ -48,11 +52,13 @@
 
         var authorLink = item.link;
 
+        // Remove the last slug from the URL
         if (item.link.slice(-1) === "/") {
           authorLink = authorLink.substring(0, authorLink.length - 1);
+          authorLink = authorLink.substr(0, authorLink.lastIndexOf("/"));
         }
 
-        item.authorLink = authorLink.substr(0, authorLink.lastIndexOf("/"));
+        item.authorLink = authorLink;
 
         return item;
 
@@ -61,6 +67,7 @@
     })
 
     // Transforms an item's published property into a readable representation
+    // in the form '8th Apr 2012 at 12:00'
     .filter('itemPublished', function($filter) {
 
       return function(item) {
@@ -68,9 +75,10 @@
         var published = new Date(item.published);
 
         if (typeof published.getMilliseconds === 'function') {
+          // Date suffix solution adapted from http://stackoverflow.com/a/24061264
           var daySuffixes = ['th', 'st', 'nd', 'rd'];
           var day = $filter('date')(published, 'dd');
-          var relevantDigits = (day < 30) ? day % 20 : day % 30; // From http://stackoverflow.com/a/24061264
+          var relevantDigits = (day < 30) ? day % 20 : day % 30;
           var daySuffix = (relevantDigits <= 3) ? daySuffixes[relevantDigits] : daySuffixes[0];
 
           item.published = day + daySuffix + $filter('date')(published, " MMM yyyy 'at' h:mm");
@@ -82,22 +90,28 @@
 
     })
 
-    // Removes unwanted HTML from an item's description property
+    // Removes unwanted HTML from an item's description property and returns an array of description lines
     .filter('itemDescription', function() {
 
       return function(item) {
 
+        // Split the description at the start of each p tag, and keep only the last
         var descriptionParts = item.description.split('<p>');
         var description = descriptionParts[descriptionParts.length - 1];
 
+        // Remove closing p tag from remaining element, if it exists
         if (description.slice(-4) === '</p>') {
           description = description.substring(0, description.length - 4);
         }
 
         var descriptionLines;
+
+        // If the remaining element contains an img tag, it's because there is no description
+        // In this case we return an empty array
         if (description.indexOf('<img ') !== -1) {
           descriptionLines = [];
         }
+        // Otherwise, we return an array of description lines after splitting on each br tag
         else {
           descriptionLines = description.split('<br />');
 
